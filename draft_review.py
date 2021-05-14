ba232d89c21439d3c159d21948bd479b1e18aac6
henry <42360223+HenREEEE@users.noreply.github.com>
2021-05-14 03:38:30 -0400

    Update draft_review.py

--- draft_review.py
+++ draft_review.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import tasks
 from os import environ
+import json
+import requests
 
 
 class MyClient(discord.Client):
@@ -8,9 +10,22 @@ class MyClient(discord.Client):
         super().__init__(*args, **kwargs)
 
         # an attribute we can access from our task
-        self.draftDict = {
+        draftDict = {
 
         }
+
+        params = {"action": "query", "list": "categorymembers", "cmtitle": "Category:Drafts_awaiting_review",
+                  "format": "json"}
+        url = 'https://2b2t.miraheze.org/'
+
+        request = requests.get(url + "w/api.php", params=params)
+        json_data = request.json()
+
+        pages = json_data['query']['categorymembers']
+        for i in range(len(pages)):
+            title = pages[i]['title']
+            link = url + "wiki/" + pages[i]['title']
+            draftDict[title] = link
 
         # start the task to run in the background
         self.my_background_task.start()
@@ -24,7 +39,6 @@ class MyClient(discord.Client):
     @tasks.loop(hours=1)  # task runs every 60 seconds
     async def my_background_task(self):
         channel = self.get_channel(842627063856103444)  # channel ID goes here
-        # self.counter += 1
         await channel.send(self.draftDict)
 
     @my_background_task.before_loop
