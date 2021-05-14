e9cd4c606a2917bf48627bf9c29d11cb6adc1492
henry <42360223+HenREEEE@users.noreply.github.com>
2021-05-14 03:39:16 -0400

    Delete jsonTest.py

--- jsonTest.py
+++
@@ -1,9 +0,0 @@
-import requests
-params = {"action": "query", "list": "categorymembers", "cmtitle": "Category:Drafts_awaiting_review", "format": "json"}
-
-request = requests.get("https://2b2t.miraheze.org/w/api.php", params=params)
-json_data = request.json()
-
-pages = json_data['query']['categorymembers']
-for i in range(len(pages)):
-    print(pages[i]['title'])
