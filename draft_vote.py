bb6abd3a52cda86bce5fecf28d7b570b4a7b8fad
kortregt <42360223+kortregt@users.noreply.github.com>
2023-09-14 11:39:13 -0400

    updated vote from prefixed to slash commands, switched from reaction-based voting to buttons

--- draft_vote.py
+++ draft_vote.py
@@ -1,37 +1,60 @@
+import asyncio
+
 import discord
 from discord.ext import commands
 
 
+class Poll(discord.ui.View):
+    yesList = []
+    noList = []
+
+    @discord.ui.button(label="Approve: 0", custom_id="approve", style=discord.ButtonStyle.green, emoji="👍")
+    async def approve_callback(self, approve, interaction: discord.Interaction):
+        user = interaction.user
+        if user not in self.yesList:
+            self.yesList.append(user)
+            if user in self.noList:
+                self.noList.remove(user)
+                self.children[1].label = f"Reject: {len(self.noList)}"
+        else:
+            self.yesList.remove(user)
+        approve.label = f"Approve: {len(self.yesList)}"
+        await interaction.response.edit_message(view=self)
+
+    @discord.ui.button(label="Reject: 0", custom_id="reject", style=discord.ButtonStyle.red, emoji="👎")
+    async def reject_callback(self, reject, interaction: discord.Interaction):
+        user = interaction.user
+        if user not in self.noList:
+            self.noList.append(user)
+            if user in self.yesList:
+                self.yesList.remove(user)
+                self.children[0].label = f"Approve: {len(self.yesList)}"
+        else:
+            self.noList.remove(user)
+        reject.label = f"Reject: {len(self.noList)}"
+        await interaction.response.edit_message(view=self)
+
+
 class DraftVote(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot):
         self.bot = bot
         self.vote_id = 0
 
-    @commands.command(name='vote')
+    @discord.application_command(name="vote", description="Starts/ends voting on a draft.")
     @commands.has_role(843007895573889024)
-    async def vote(self, ctx: commands.Context, status, name, title):
-        await ctx.message.delete()
-        if status == 'start':
-            page = discord.Embed(title=f"Vote for Draft: {title} by {name}", description="",
-                                 color=discord.Color.from_rgb(36, 255, 0))
-            message = await ctx.send(embed=page)
-            await message.add_reaction('🟢')
-            await message.add_reaction('🔴')
-            self.vote_id = message.id
-        if status == 'end':
-            nmsg = await ctx.channel.fetch_message(self.vote_id)
-            reactions = nmsg.reactions
-
-            final_count = discord.Embed(title=f"Results for Draft: {title} by {name}",
-                                        color=discord.Color.from_rgb(36, 255, 0))
-            if reactions[0].count > reactions[1].count:
-                final_count.description = f'Approved\n{str(reactions[0])} **| {str(reactions[0].count - 1)}**\n' \
-                                          f'\n{str(reactions[1])}  **|  {str(reactions[1].count - 1)}**'
-            else:
-                final_count.description = f'Rejected\n{str(reactions[0])} **| {str(reactions[0].count - 1)}**\n' \
-                                          f'\n{str(reactions[1])}  **|  {str(reactions[1].count - 1)}**'
-            await ctx.send(embed=final_count)
+    async def vote(self, interaction: discord.Interaction, name, title):
+        await interaction.response.defer()
+        page = discord.Embed(title=f"Vote for Draft: {title} by {name}",
+                             color=discord.Color.from_rgb(36, 255, 0))
+        poll = Poll()
+        message = await interaction.followup.send(embed=page, view=poll)
+        # await asyncio.sleep(5)  # testing purposes
+        await asyncio.sleep(86400)  # one day
+        poll.children[0].disabled = True
+        poll.children[1].disabled = True
+        page.title = f"Vote for Draft: {title} by {name} has ended"
+        await message.edit(embed=page, view=poll)
 
 
-def setup(bot: commands.Bot):
+def setup(bot):
     bot.add_cog(DraftVote(bot))
