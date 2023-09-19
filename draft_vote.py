67575da162dcf92137233ac942c24acafd695300
kortregt <42360223+kortregt@users.noreply.github.com>
2023-09-19 03:38:03 -0400

    should be fixed

--- draft_vote.py
+++ draft_vote.py
@@ -22,7 +22,7 @@ class Poll(discord.ui.View):
         else:
             self.yesList.remove(user)
         approve.label = f"Approve: {len(self.yesList)}"
-        await interaction.response.edit_message(view=self) # change edit_messages to followups.
+        await interaction.response.edit_message(view=self)  # change edit_messages to followups.
 
     @discord.ui.button(label="Reject: 0", custom_id="reject", style=discord.ButtonStyle.red, emoji="👎")
     async def reject_callback(self, reject, interaction: discord.Interaction):
@@ -35,11 +35,14 @@ class Poll(discord.ui.View):
         else:
             self.noList.remove(user)
         reject.label = f"Reject: {len(self.noList)}"
-        await interaction.response.edit_message(view=self) # change edit_messages to followups.
+        await interaction.response.edit_message(view=self)  # change edit_messages to followups.
+
 
 """
 Message that sends the button to view the Modal.
 """
+
+
 class ModalMessage(discord.ui.View):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
@@ -51,8 +54,8 @@ class ModalMessage(discord.ui.View):
 
     @discord.ui.button(label="Give input for Draft", style=discord.ButtonStyle.green)
     async def button_callback(self, button, interaction: discord.Interaction):
-        modal = VoteModal(title=self.title) # Object within and Object. Objectception.
-        modal.draftName = self.draftName # gotta parse this shit through
+        modal = VoteModal(title=self.title)  # Object within and Object. Objectception.
+        modal.draftName = self.draftName  # gotta parse this shit through
         modal.draftTitle = self.draftTitle
         modal.bot = self.bot
 
@@ -70,13 +73,13 @@ class DraftVote(commands.Cog):
         self.bot = bot
         self.vote_id = 0
 
-    async def sendModal(self, interaction: discord.Interaction, poll: Poll, name, title):
+    async def sendModal(self, interaction: discord.Interaction, poll: Poll, name, title, bot):
         modal = ModalMessage()
         modal.draftName = name
         modal.draftTitle = title
         modal.bot = self.bot
 
-        if len(poll.yesList) >= len(poll.noList): # Not sure on rules here, will need to deliberate.
+        if len(poll.yesList) >= len(poll.noList):  # Not sure on rules here, will need to deliberate.
             modal.title = "Accepted"
             modal.accepted = True
             await interaction.followup.send(view=modal, ephemeral=True)
@@ -84,10 +87,8 @@ class DraftVote(commands.Cog):
             modal.title = "Rejected"
             await interaction.followup.send(view=modal, ephemeral=True)
 
-        
-
     @discord.commands.application_command(name="vote", description="Starts/ends voting on a draft.")
-    #@commands.has_role(843007895573889024)
+    # @commands.has_role(843007895573889024)
     @discord.option("duration", float, description="Length of the vote in hours", required=False)
     @discord.option("name", str, description="Author of the draft", required=True)
     @discord.option("title", str, description="Title of the draft", required=True)
@@ -95,52 +96,62 @@ class DraftVote(commands.Cog):
         await interaction.response.defer()
         page = discord.Embed(title=f"Vote for Draft: {title} by {name}",
                              color=discord.Color.from_rgb(36, 255, 0),
-                             description=("Vote end: <t:" + str(round(time.time()+duration*3600))) + ":R>")
-        
-        poll = Poll() # Create Poll object
-        message = await interaction.followup.send(embed=page, view=poll) # Parse Poll and embed onto a message, and send it.
+                             description=("Vote end: <t:" + str(round(time.time() + duration * 3600))) + ":R>")
+
+        poll = Poll()  # Create Poll object
+        message = await interaction.followup.send(embed=page,
+                                                  view=poll)  # Parse Poll and embed onto a message, and send it.
         # await asyncio.sleep(5)  # testing purposes
-        await asyncio.sleep(duration*3600) # duration parsed through
-        poll.children[0].disabled = True # Disable both buttons. Luckily, they are the only children.
+        await asyncio.sleep(duration * 3600)  # duration parsed through
+        poll.children[0].disabled = True  # Disable both buttons. Luckily, they are the only children.
         poll.children[1].disabled = True
         page.title = f"Vote for Draft: {title} by {name} has ended"
-        # TODO: If we want to have the one who started the poll pinged, we will need to do this differenty.
-        #guild = self.bot.get_guild(697848129185120256) # get role from discord server
-        #role = guild.get_role(843007895573889024)
-        await message.edit(embed=page, view=poll) # edit the poll with the buttons disabled.
+        # TODO: If we want to have the one who started the poll pinged, we will need to do this differently.
+        # guild = self.bot.get_guild(697848129185120256) # get role from discord server
+        # role = guild.get_role(843007895573889024)
+        await message.edit(embed=page, view=poll)  # edit the poll with the buttons disabled.
 
-        #await interaction.followup.send(role.mention, allowed_mentions=discord.AllowedMentions.all()) # send a mention to the role.
-        
+        # await interaction.followup.send(role.mention, allowed_mentions=discord.AllowedMentions.all()) # send a mention to the role.
+
         # TODO: instead of a mention to the role, send a MODAL depending on Rejected / Accepted.
         await self.sendModal(interaction, poll, name, title, self.bot)
+
 
 """
 Create Modal for user interaction after vote completion
 """
+
+
 class VoteModal(discord.ui.Modal):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.accepted = False # Defaulted to false, changed to True in ModalMesage() class.
+        self.accepted = False  # Defaulted to false, changed to True in ModalMesage() class.
         self.draftName = ""
         self.draftTitle = ""
         self.bot = commands.Bot
 
     async def callback(self, interaction: discord.Interaction):
+        await interaction.response.defer()
         textInput = self.children[0].value  # this is what the user has inputted into the textbox.
         if self.accepted:
             # TODO: what to do if the Modal is an accepted Modal
-            await self.bot.get_cog('DraftBot') #.approve(self.draftName, self.draftTitle, textInput)
-            print(f"ACCEPTED {0} by {1}".format(self.draftName, self.draftTitle))
+            await self.bot.get_cog('DraftBot').approve(self.draftName, self.draftTitle, textInput)
+            print("ACCEPTED {0} by {1}".format(self.draftName, self.draftTitle))
+            embed = discord.Embed(title=f"Draft:{self.draftTitle} by {self.draftName} approved!")
+            embed.url = f"https://2b2t.miraheze.org/wiki/{self.draftTitle}"
+            await interaction.response.send(embed=embed)
         else:
             # TODO: what to do if the Modal is a rejected Modal.
-            await draft_review.DraftBot.reject(self.draftName, self.draftTitle, textInput)
-            print(f"REJECTED {0} by {1}".format(self.draftName, self.draftTitle))
+            await self.bot.get_cog('DraftBot').reject(self.draftName, self.draftTitle, textInput)
+            print("REJECTED {0} by {1}".format(self.draftName, self.draftTitle))
+            embed = discord.Embed(title=f"Draft:{self.draftTitle} by {self.draftName} rejected.")
+            embed.url = f"https://2b2t.miraheze.org/wiki/User:{self.draftName}/Drafts/{self.draftTitle}"
+            await interaction.followup.send(embed=embed)
 
-
-        #embed = discord.Embed(title="Modal Results")
-        #embed.add_field(name="Short Input", value=self.children[0].value)
-        #embed.add_field(name="Long Input", value=self.children[1].value)
-        #await interaction.response.send_message(embeds=[embed])
+        # embed = discord.Embed(title="Modal Results")
+        # embed.add_field(name="Short Input", value=self.children[0].value)
+        # embed.add_field(name="Long Input", value=self.children[1].value)
+        # await interaction.response.send_message(embeds=[embed])
 
 
 def setup(bot):
